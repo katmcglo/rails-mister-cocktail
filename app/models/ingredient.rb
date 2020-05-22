03e360057f5a86ceb520e3a87fcbb64cac4f634d
Kat mcg <cathaumann@gmail.com>
2020-05-22 18:16:55 +0200

    got the cocktails displaying doses

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,7 @@
 class Ingredient < ApplicationRecord
-  has_many :doses, class_name: "dose", foreign_key: "dose_id"
+  has_many :doses
+  has_many :cocktails, through: :doses
+
+  validates :name, presence: true, uniqueness: true
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "zimt")
+Ingredient.create(name: "cointreau")
+Ingredient.create(name: "chili")
+
+Dose.destroy_all
+Dose.create(description: "100ml")
